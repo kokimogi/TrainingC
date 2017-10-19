be9a50fb4302825e96b010d1b92244056d54d7fb
kokimogi <kokko1035@gmail.com>
2017-10-19 16:40:06 +0900

    training

---
+++ list06.c
@@ -0,0 +1,20 @@
+/*
+ * @file  list6.c
+ * @brief 二つの変数に整数値を格納して表示
+ * @author mogi
+ * @date 2017/10/18
+*/
+
+#include <stdio.h>
+
+int main(void){
+	int vx ,vy;
+
+	vx = 57;
+
+	vy = vx + 10;
+
+	printf("vxの値は%dです。\n",vx );
+
+	printf("vyの値は%dです。\n",vy );
+}
