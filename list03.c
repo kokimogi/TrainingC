c73c40c9045581c601b94cc519604950a8b910fb
kokimogi <kokko1035@gmail.com>
2017-10-19 16:33:31 +0900

    hogehoge

---
+++ list03.c
@@ -0,0 +1,14 @@
+/*
+ * @file  list3.c
+ * @brief 整数値15と37を加えた結果を丁寧に表示
+ * @author mogi
+ * @date 2017/10/18
+*/
+
+#include <stdio.h>
+
+int main(void){
+	printf("15と37の和は$dです。\n",15+37);
+
+	return 0;
+}
