99c39387005c9770e44aecabb4c6b62b543bc396
kokimogi <kokko1035@gmail.com>
2017-10-19 16:40:10 +0900

    training

---
+++ list07.c
@@ -0,0 +1,18 @@
+/*
+ * @file  list7.c
+ * @brief 二つの変数を初期化して表示
+ * @author mogi
+ * @date 2017/10/18
+*/
+
+#include <stdio.h>
+
+int main(void){
+	int vx = 57;
+	int vy = vx + 10;
+
+	printf("vxの値は%dです。\n",vx );
+	printf("vyの値は%dです。\n",vy );
+
+	return 0;
+}
