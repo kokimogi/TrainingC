6579777895189c3aaf9a8948bf430fb4f321661a
kokimogi <kokko1035@gmail.com>
2017-10-25 23:48:51 +0900

    MyCommit

---
+++ list18.c
@@ -0,0 +1,19 @@
+/*
+ * @file  list18.c
+ * @brief　読み込んだ整数値は、5で割り切れないか
+ * @author mogi
+ * @date 2017/10/23
+*/
+
+#include <stdio.h>
+
+int main(void){
+	int no;
+
+	printf("整数を入力してください:");
+	scanf("%d",&no);
+
+	if(no % 5)
+		puts("その数は5で割り切れません。");
+	return 0;
+}
